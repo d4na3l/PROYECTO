901072035f8d3b61784655e631951820632f589a
daniel <christian.daniel.ve@gmail.com>
2023-07-27 22:15:14 -0400

    fix: enrouting controllers fix

--- web/app/core/Controller.php
+++ web/app/core/Controller.php
@@ -4,6 +4,7 @@ class Controller
 {
     public function view($name)
     {
+
         $filename = "../app/views/" . $name . ".view.php";
         if (file_exists($filename)) {
             require $filename;
--- web/app/views/404.view.php
+++ web/app/views/404.view.php
@@ -1,1 +1,2 @@
 <h1>View file not found</h1>
+<h2>404 ERROR</h2>
