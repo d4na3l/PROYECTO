dab9d6f33473ff0dd260b2b43e7476df7b3e3b64
daniel <christian.daniel.ve@gmail.com>
2023-07-29 15:07:40 -0400

    feat: add conection to bd

--- web/app/core/Database.php
+++ web/app/core/Database.php
@@ -1,16 +1,33 @@
 <?php
-$password = "root";
-$user = "root";
-$dbname = "project_u";
-$host = "127.0.0.1";
-$port = "5432";
 
+trait Database
+{
+    private function connect()
+    {
+        $string = 'pgsql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME;
 
-$string = 'psql:host=$host;port=$port;dbname=$dbname';
+        try {
+            $con = new PDO($string, DB_USER, DB_PSW);
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $con;
+        } catch (Exception $e) {
+            echo 'Failure to connect to db: ' . $e->getMessage();
+        }
+    }
 
-try {
-    $con = new PDO($string, $user, $password);
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    echo 'Failure to connect to db: ' . $e->getMessage();
+    public function query($query, $data = [])
+    {
+        $con = $this->connect();
+        $stm = $con->prepare($query);
+
+        $check = $stm->execute($data);
+        if($check){
+            $result = $stm->fetchAll(PDO::FETCH_OBJ);
+            if(is_array($result) && count($result)){
+                return $result;
+            }
+        }
+
+        return false;
+    }
 }
--- web/app/core/config.php
+++ web/app/core/config.php
@@ -1,13 +1,15 @@
 <?php
-if($_SERVER['SERVER_NAME']  == 'localhost')
-{
+if ($_SERVER['SERVER_NAME']  == 'localhost') {
+    /**database config */
+    define('DB_NAME', 'project_u');
+    define('DB_HOST', 'db');
+    define('DB_PORT', '5432');
+    define('DB_USER', 'root');
+    define('DB_PSW', 'root');
+    define('DB_DRIVER', '');
+
+
     define('ROOT', 'http://localhost/public');
-}else
-{
+} else {
     define('ROOT', 'http://www.youwebsite.com');
 }
-
-define('DB_HOST', 'localhost:5432');
-define('DB_NAME', 'project_u');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
