30527ecc00f8e32403b2551e408d97934d389b2c
Jesus <gsusvivas018@gmail.com>
2023-07-31 23:00:24 -0400

    feat: new login page design

--- web/app/views/login/login.php
+++ web/app/views/login/login.php
@@ -4,19 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="icon" href="assets/img/logo.jpg">
+    <link rel="icon" href="assets/img/logo.png">
     <title>Login</title>
 </head>
 <body>
     <div class="section">
-        <div class="container animacion">
-        <h1 class="section__title animacion">Login</h1>
+        <div class="container container-hidden animation-left">
+            <img class="logo" src="assets/img/logo.png" alt="Logo institucional">
+        </div>
+        <div class="container animation-right">
+        <h1 class="section__title animation-right">Login</h1>
         <form method="POST" action="#">
             <div class="usuario">
-                <input class="form animacion" type="text" placeholder="Usuario">
-                <input class="form animacion" type="password" placeholder="Contraseña">
+                <input class="form animation-right-input" type="text" placeholder="Usuario">
+                <input class="form animation-right-input" type="password" placeholder="Contraseña">
             </div>
-            <input type="submit" class="boton animacion" value="Ingresar">
+            <input type="submit" class="boton animation-right-input" value="Ingresar">
         </form>
         </div>
     </div>
