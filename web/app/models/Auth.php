8f5e45a942fb1fb695f8023989025ff2653d2937
daniel <chris.daniel.ve@gmail.com>
2023-08-22 15:49:46 -0400

    fix: data schema sql fix, avances con el login.js y el Auth.php

--- web/app/models/Auth.php
+++ web/app/models/Auth.php
@@ -4,16 +4,24 @@ class Auth
     use User;
     public function auth($post)
     {
+        if (!is_numeric($post['ci'])) {
+            $error = array(
+                        'status'=>'unknown',
+                        'description'=>'',
+                        'status'=>'',
+                        'conection'=>'false',
+                        );
+            return $error;
+        }
         $arr['ci'] = $post['ci'];
         $login = $this->first($arr);
         if (!empty($login)) {
             $hashedPassword = $login->password;
             if (password_verify($post['password'], $hashedPassword)) {
-                $_SESSION['id'] = $login->id;
-                $_SESSION['session'] = true;
-                $_SESSION['user'] = $login->ci;
-                location('dashboard');
+                return $login;
+                // location('dashboard');
             } else {
+                // show("password n't");
                 location('login');
             }
         } else {
--- web/public/assets/js/login.js
+++ web/public/assets/js/login.js
@@ -1,53 +1,49 @@
 $("document").ready(() => {
-    let passwordErrors;
-    let ciErrors;
-    $("input[name='ci']").on("input", (e) => {
-        ciErrors = [];
-        if (!Number($(e.target).val())) {
-            ciErrors.push("Este campo solo debe ser llenado con números");
-        }
-        const ciValue = $(e.target).val().replace(/\D/g, "");
-        $(e.target).val(ciValue);
-    });
+  let passwordErrors;
+  let ciErrors;
+  $("input[name='ci']").on("input", (e) => {
+    ciErrors = [];
+    if (!Number($(e.target).val())) {
+      ciErrors.push("Este campo solo debe ser llenado con números");
+    }
+    const ciValue = $(e.target).val().replace(/\D/g, "");
+    $(e.target).val(ciValue);
+  });
 
-    $("input[name='password']").on("input", () => {
-        const passwordValue = $("input[name='password']").val();
+  $("input[name='password']").on("input", () => {
+    const passwordValue = $("input[name='password']").val();
 
-        const regexUpperCase = /(?=.*[A-Z])/;
-        const regexLowerCase = /(?=.*[a-z])/;
-        const regexNumberCase = /(?=.*\d)/;
-        const regexSymbolCase = /(?=.*[.;*(/$&)])/;
-        const regexCatchRange = /[\w.;*(/$&)]{8,}/;
+    const regexUpperCase = /(?=.*[A-Z])/;
+    const regexLowerCase = /(?=.*[a-z])/;
+    const regexNumberCase = /(?=.*\d)/;
+    const regexSymbolCase = /(?=.*[.;*(/$&)])/;
+    const regexCatchRange = /[\w.;*(/$&)]{8,}/;
 
-        passwordErrors = [];
+    passwordErrors = [];
 
-        if (!regexUpperCase.test(passwordValue)) {
-            passwordErrors.push(
-                "La contraseña debe contener al menos una letra mayúscula.",
-            );
-        }
-        if (!regexLowerCase.test(passwordValue)) {
-            passwordErrors.push(
-                "La contraseña debe contener al menos una letra minúscula.",
-            );
-        }
-        if (!regexNumberCase.test(passwordValue)) {
-            passwordErrors.push(
-                "La contraseña debe contener al menos un número.",
-            );
-        }
-        if (!regexSymbolCase.test(passwordValue)) {
-            passwordErrors.push(
-                "La contraseña debe contener al menos uno de los siguientes símbolos: . ; * ( / $ & ).",
-            );
-        }
-        if (!regexCatchRange.test(passwordValue)) {
-            passwordErrors.push(
-                "La contraseña debe tener al menos 8 caracteres.",
-            );
-        }
-        // Puedes ver el arrego de passwordErrors en consola del navegador
-        /* haz que los elementos de passwordErrors se impriman en la vista del login, en algun modal. Ve si puedes implementarlo
+    if (!regexUpperCase.test(passwordValue)) {
+      passwordErrors.push(
+        "La contraseña debe contener al menos una letra mayúscula."
+      );
+    }
+    if (!regexLowerCase.test(passwordValue)) {
+      passwordErrors.push(
+        "La contraseña debe contener al menos una letra minúscula."
+      );
+    }
+    if (!regexNumberCase.test(passwordValue)) {
+      passwordErrors.push("La contraseña debe contener al menos un número.");
+    }
+    if (!regexSymbolCase.test(passwordValue)) {
+      passwordErrors.push(
+        "La contraseña debe contener al menos uno de los siguientes símbolos: . ; * ( / $ & )."
+      );
+    }
+    if (!regexCatchRange.test(passwordValue)) {
+      passwordErrors.push("La contraseña debe tener al menos 8 caracteres.");
+    }
+    // Puedes ver el arrego de passwordErrors en consola del navegador
+    /* haz que los elementos de passwordErrors se impriman en la vista del login, en algun modal. Ve si puedes implementarlo
 
         - La validaciones previstas aseguran que no se pueda enviar el formulario si los campos no estan llenos
         - Tampoco se pueden ingresar dentro de campo de cedulas otro caracter que no sea numero
@@ -57,35 +53,37 @@ $("document").ready(() => {
 
         Estas validaciones solo nos ayuarán
         */
-    });
+  });
 
-    $("#loginForm").submit((e) => {
-        e.preventDefault();
-        const ciValue = $("input[name='ci']").val();
-        const passwordValue = $("input[name='password']").val();
+  $("#loginForm").submit((e) => {
+    // e.preventDefault();
+    const ciValue = $("input[name='ci']").val();
+    const passwordValue = $("input[name='password']").val();
 
-        if (
-            !ciValue ||
-            !passwordValue ||
-            passwordErrors.length ||
-            ciErrors.length
-        ) {
-            // Añadir los mensajes correspondientes a no tener los campos llenos bien.
-        } else {
-            $("span").text("");
-            $.ajax({
-                method: "POST",
-                data: {
-                    ci: ciValue,
-                    password: passwordValue,
-                },
-                success: (res) => {
-                        console.log(res);
-                },
-                error: () => {
-                    console.log("Ha ocurrido un error durante la ejecución");
-                },
-            });
-        }
-    });
+    if (
+      !ciValue ||
+      !passwordValue ||
+      passwordErrors.length ||
+      ciErrors.length
+    ) {
+      console.log("password faults:", passwordErrors);
+      console.log("cedula fault:", ciErrors);
+      // Añadir los mensajes correspondientes a no tener los campos llenos bien.
+    } // else {
+    //     $("span").text("");
+    //     $.ajax({
+    //         method: "POST",
+    //         data: {
+    //             ci: ciValue,
+    //             password: passwordValue,
+    //         },
+    //         success: (res) => {
+    //                 console.log('conectado');
+    //         },
+    //         error: () => {
+    //             console.log("Ha ocurrido un error durante la ejecución");
+    //         },
+    //     });
+    // }
+  });
 });
