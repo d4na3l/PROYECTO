9700b1d042f7670b91f5e711b22cbdadbb384045
Jesus <gsusvivas018@gmail.com>
2023-08-27 02:42:10 -0400

    fixed: Se modificaron las vistas y funciones de alertas en los archivos login, signup, y 404

--- web/app/views/404.view.php
+++ web/app/views/404.view.php
@@ -3,15 +3,17 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/404.css">
     <link rel="icon" href="<?= ROOT ?>/assets/img/logo.png">
     <title>CM Chacao - 404</title>
 </head>
 
 <body>
-    <h1>View file not found</h1>
-    <h2>404 ERROR</h2>
-    <button><a href="<?= ROOT ?>/dashboard">Back home</a></button>
+    <h1 class="section__title">View file not found</h1>
+    <h2 class="section__subtitle">404 ERROR</h2>
+    <a href="<?= ROOT ?>/dashboard">Back home</a>
 </body>
 
 </html>
